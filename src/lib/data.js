50e9cf37614800257e322f6de980b703ddcc363f
Josh Newlin <josh.b.newlin@gmail.com>
2026-04-25 21:02:54 +0100

    Update themeing

--- src/lib/data.js
+++ src/lib/data.js
@@ -2,12 +2,13 @@
 // DynamoDB and are loaded via $lib/server/content.js + +page.server.js.
 
 export const THEMES = {
-  '':         { name: 'Drylands',        swatches: ['#c89830', '#1a1408', '#d64040', '#7a3080'] },
-  'forest':   { name: 'Midnight Forest', swatches: ['#4a9fd8', '#0f1620', '#d64040', '#7a5ab0'] },
+  '':         { name: 'Drylands',        swatches: ['#e85f35', '#1f1614', '#ff7a5a', '#b8845a'] },
+  'sunset':   { name: 'Sunset Over Dunes', swatches: ['#d97e3a', '#1a1410', '#ff6a4a', '#8a5a9a'] },
+  'forest':   { name: 'Midnight Forest', swatches: ['#5aafdd', '#0f1518', '#d64040', '#8a8acc'] },
+  'light':    { name: 'Light Desert',    swatches: ['#c94d2c', '#f9f3ea', '#d84a40', '#8a5a9a'] },
   'infernal': { name: 'Infernal',        swatches: ['#ff6b20', '#2a1008', '#ff6b6b', '#c85a7a'] },
-  'frost':    { name: 'Frostborn',       swatches: ['#6ac8ff', '#0d1a28', '#d64040', '#8a7ab0'] },
+  'frost':    { name: 'Frostborn',       swatches: ['#6ad0ff', '#0a1620', '#d64040', '#9a88cc'] },
   'royal':    { name: 'Royal Purple',    swatches: ['#b080ff', '#141025', '#d64040', '#a860d0'] },
-  'light':    { name: 'Light Mode',      swatches: ['#c88830', '#ffffff', '#e04848', '#b070d8'] },
   'ocean':    { name: 'Ocean Deep',      swatches: ['#2abaff', '#0a1428', '#d64040', '#7a8ab0'] },
   'autumn':   { name: 'Autumn Rustic',   swatches: ['#d8880a', '#261608', '#d86050', '#9a6030'] },
   'cyber':    { name: 'Cyberpunk',       swatches: ['#ff00ff', '#141422', '#ff1a6a', '#9900ff'] },
